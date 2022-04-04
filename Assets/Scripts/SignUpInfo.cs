ec22affa4cd92904955cb6b2051d2cc0f6e99cb9
mol1986@naver.com <mol1986@naver.com>
2022-04-04 15:51:21 +0900

    아이디,비밀번호 체크

--- Assets/Scripts/SignUpInfo.cs
+++ Assets/Scripts/SignUpInfo.cs
@@ -1,18 +1,169 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SignUpInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TMP_InputField fieldID;
+    [SerializeField] TMP_InputField fieldPW;
+    [SerializeField] TMP_InputField fieldPWRe;
+
+    [SerializeField] TMP_Text errorTextID;      // 아이디의 에러 텍스트.
+    [SerializeField] TMP_Text errorTextPW;
+
+    bool isValidID;      // 아이디가 유효한가?   (4자 이상, 알파벳+숫자 조합)
+    bool isNotOverlapID; // 아이디가 중복되는가? (실제로 백엔드 서버에 중복 아이디가 존재하는가?)
+
+    bool isValidPW;      // 비밀번호가 유효한가? (8 ~ 16자, 알파벳+숫자+특수문자 조합. 대문자 포함.)
+    bool isEqualPW;      // 비밀번호가 동일한가?
+
+    private void OnEnable()
     {
-        
+        errorTextID.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    #region 아이디
+
+    public void OnChangedID(TMP_InputField textID)
     {
-        
+        // 아이디를 변경했기 때문에 유효성과 체크 유무를 초기화 시킨다.
+        isValidID = false;
+        isNotOverlapID = false;
+
+        errorTextID.gameObject.SetActive(false);
     }
+    public void OnEndEditID(TMP_InputField textID)
+    {          
+        // 아이디는 최소 4글자 이상, 알파벳 + 숫자만 가능.
+        string id = textID.text;
+        IsCheckValiedID(id);
+    }
+    public void OnDoubleCheckID(TMP_InputField textID)
+    {
+        IsCheckValiedID(textID.text);           // 유효성 체크.
+        if(!isValidID)                          // 유효하지 않다면.
+            return;                             // 중복체크 X.
+
+        // 입력한 아이디의 중복 체크를 시킨다.
+        string id = textID.text;
+        WebManager.Instance.Post(CallbackDoubleCheckID, "doubleCheckID", new StringFair("id", id));
+    }
+    private void CallbackDoubleCheckID(WebManager.WebData data)
+    {
+        // 실제로 웹에서 받은 결과 값을 토대로 변수 세팅.
+        isNotOverlapID = string.Equals(data.result, "ok");
+
+        errorTextID.gameObject.SetActive(true);
+        errorTextID.text = isNotOverlapID ? "사용 가능한 아이디입니다." : "중복된 아이디입니다.";
+        errorTextID.color = isNotOverlapID ? Color.green : Color.red;
+    }
+
+    private void IsCheckValiedID(string id)
+    {
+        string text = string.Empty;
+        bool isValid = true;
+
+        if (string.IsNullOrEmpty(id))
+        {
+            text = "아이디가 입력되지 않았습니다.";
+            isValid = false;
+        }
+        else if (id.Length < 4)
+        {
+            text = "아이디는 최소 4글자 이상입니다.";
+            isValid = false;
+        }
+        else if (!IsEnglisOrNumber(id))
+        {
+            text = "아이디는 알파벳 + 문자의 조합이어야만 합니다.";
+            isValid = false;
+        }
+
+        if (!isValid)
+        {
+            errorTextID.gameObject.SetActive(true);
+            errorTextID.text = text;
+        }
+    }
+    private bool IsEnglisOrNumber(string id)
+    {
+        // id에 영어와 숫자만 포함되어 있는지 확인하는 함수.
+        foreach (char c in id)
+        {
+            if (!(IsEnglish(c) || IsNumeric(c)))
+                return false;
+        }
+
+        return true;
+    }
+
+    bool IsKorean(char ch)
+    {
+        return (0xAC00 <= ch && ch <= 0xD7A3) || (0x3131 <= ch && ch <= 0x318E);
+    }
+    bool IsNumeric(char ch)
+    {
+        return (0x30 <= ch && ch <= 0x39);
+    }
+    bool IsEnglish(char ch)
+    {
+        return (0x61 <= ch && ch <= 0x7A) || (0x41 <= ch && ch <= 0x5A);
+    }
+
+    #endregion
+
+    #region 비밀번호
+
+    public void OnChangedPW()
+    {
+        isValidPW = false;
+        isEqualPW = false;
+        errorTextPW.gameObject.SetActive(false);
+    }
+
+    private void IsCheckValidPW(string pw)
+    {
+        //(8 ~ 16자, 알파벳+숫자+특수문자 조합. 대문자 포함.)
+        string text = string.Empty;
+        if (pw.Length < 8 || 16 < pw.Length)
+        {
+            text = "8 ~ 16자, 알파벳+숫자+특수문자 조합. 대문자 포함.";
+            isValidPW = false;
+        }
+
+        errorTextPW.gameObject.SetActive(!isValidPW);
+        errorTextPW.color = Color.red;
+        errorTextPW.text = text;
+    }
+    public void OnEndEditPW()
+    {
+        string pw = fieldPW.text;
+        IsCheckValidPW(pw);
+    }
+    public void OnEndEditPWRe()
+    {
+        errorTextPW.gameObject.SetActive(true);
+        IsCheckValidPW(fieldPWRe.text);
+        
+        if(!isValidPW)
+        {
+        }
+        else if(string.Equals(fieldPW.text, fieldPWRe.text))
+        {
+            errorTextPW.text = "비밀번호가 동일합니다.";
+            errorTextPW.color = Color.green;
+        }
+        else
+        {
+            errorTextPW.text = "비밀번호가 동일하지 않습니다.";
+            errorTextPW.color = Color.red;
+        }
+    }
+
+
+    #endregion
+
+
 }
---
+++ Assets/Scripts/WebManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public struct StringFair
+{
+    public string Key { get; private set; }
+    public string Value { get; private set; }
+
+    public StringFair(string key, string value)
+    {
+        this.Key = key;
+        this.Value = value;
+    }
+}
+
+public class WebManager : MonoBehaviour
+{
+    #region 상수
+
+    private const string URL = "https://script.google.com/macros/s/AKfycbwnbZFBuqJZPEUruCK41JYBiRlgZGCLylK5DD2J2n1XL7C71b8gVZCgP9V0KekgSypx9w/exec";
+    static WebManager instance;
+    public static WebManager Instance => instance;
+
+    [System.Serializable] public class WebData
+    {
+        public string order;
+        public string result;
+        public string msg;
+        public int value;
+    }
+
+    public delegate void Callback(WebData data);   // WebData 자료형을 매개변수로 받는 델리게이트.
+
+    #endregion
+
+    private bool isNetworking;
+    Callback callback;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public bool Post(Callback callback, string order, params StringFair[] postDatas)
+    {
+        if (isNetworking)
+            return false;
+
+        this.callback = callback;
+
+        WWWForm form = new WWWForm();
+        form.AddField("order", order);
+
+        foreach (StringFair data in postDatas)
+            form.AddField(data.Key, data.Value);
+
+        StartCoroutine(WebPost(form));
+        return true;
+    }
+    private IEnumerator WebPost(WWWForm form)
+    {
+        isNetworking = true;
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
+            yield return www.SendWebRequest();
+
+            // 웹으로부터 응답이 왔다면.
+            if (www.isDone)
+            {
+                // 웹에서 내려받은 문자열 데이터를 Json을 이용해 WebData객체로 변환.
+                // 객체를 Callback으로 리턴.
+                string json = www.downloadHandler.text;
+                WebData webData = (WebData)JsonUtility.FromJson(json, typeof(WebData));
+                callback?.Invoke(webData);
+                Debug.Log($"Callback : {webData.msg}");
+            }
+            else
+            {
+                callback?.Invoke(null);
+                Debug.Log($"Callback : 실패");
+            }
+
+            isNetworking = false;
+        }
+    }
+}
